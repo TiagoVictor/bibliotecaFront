3dbbede9ef97444c6f185a93a827078c30677969
Tiago <tiagovictordeoliveira@gmail.com>
2023-09-15 10:26:00 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Teste!</p>
+        <p>Teste!</p>
+        <p>Teste!</p>
+        <p>Teste!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
