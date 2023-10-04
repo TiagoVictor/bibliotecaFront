09aedfc2c0b0f7c38f8602c1f53dbef56e6f73e1
Tiago V. de Oliveira <Tiago V. de Oliveira@DESKTOP-H0ESDVC>
2023-10-04 02:04:38 -0300

    Adicionando a rota para pagina de livros

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -10,6 +10,7 @@ import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Privacy from "../pages/Privacy";
 import Book from "../pages/Book";
+import EditBook from "../pages/EditBook";
 
 const AppRoutes = () => {
     return (
@@ -21,6 +22,7 @@ const AppRoutes = () => {
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/privacy" element={<Privacy />} />
                 <Route path="/book" element={<Book />} />
+                <Route path="/edit-book/:id" element={<EditBook />} />
             </Routes>
         </BrowserRouter>
     )
